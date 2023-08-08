766e91da30d363e9dac8766b0a602426e028d875
Susanistikis <susanam909@gmail.com>
2023-08-08 17:52:45 +0200

    Co-authored-by: Amalia Divina Ponce toledo <amaliaPonce@users.noreply.github.com>

--- src/controllers/exercises/filterExercises.js
+++ src/controllers/exercises/filterExercises.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const getDb = require("../../db/getDb");
 
 async function filterExercises(req, res) {
-  const { search, muscleGroup, favoritos } = req.query;
+  const { name, muscleGroup, favoritos } = req.query;
   const user_id = req.user.id;
 
   if (Object.keys(req.query).length === 0) {
@@ -15,40 +15,24 @@ async function filterExercises(req, res) {
 
     // Filtrar ejercicios
     let query = "SELECT * FROM exercises";
-
     let queryParams = [];
-    // /exercises?search="sentadillas" filtrar por palabra que este en nombre y descripción.
-    if (search) {
-      query += " WHERE name LIKE ? OR description LIKE ?";
-      queryParams.push(`%${search}%`);
-      queryParams.push(`%${search}%`);
-    }
-    // /exercises?favoritos="no" filtrar por si no esta en favoritos.
-    else if (favoritos === "si") {
+    if (favoritos) {
+      query += " WHERE id = ?";
+      queryParams.push(favoritos);
+    } else if (name) {
+      query += " WHERE name LIKE ?";
+      queryParams.push(`%${name}%`);
+    } else if (user_id) {
       query =
-        "SELECT e.* FROM exercises e LEFT JOIN favorites f ON e.id = f.exercise_id WHERE f.user_id IS NULL OR f.user_id != ?";
+        "SELECT e.* FROM exercises e JOIN favorites f ON e.id = f.exercise_id WHERE f.user_id = ?";
       queryParams.push(user_id);
-    }
-    // /exercises?grupo="inferior"&favoritos="no" que filtre por grupo muscular y si no esta en favoritos.
-    else if (muscleGroup && favoritos === "no") {
-      query =
-        "SELECT e.* FROM exercises e LEFT JOIN favorites f ON e.id = f.exercise_id WHERE muscleGroup = ? AND (f.user_id IS NULL OR f.user_id != ?)";
-      queryParams.push(muscleGroup);
-      queryParams.push(user_id);
-    }
-    // /exercises?grupo="inferior" nos filtra por el grupo muscular al que pertenece.
-    else if (muscleGroup) {
+    } else if (muscleGroup) {
       query += " WHERE muscleGroup = ?";
       queryParams.push(muscleGroup);
     }
 
-    query += " ORDER BY name";
-
-    const [result] = await connection.query(query);
-    res.status(200).send({
-      status: "ok",
-      data: result,
-    });
+    const [result] = await connection.query(query, queryParams);
+    return res.status(200).json(result);
   } catch (err) {
     console.error(err);
     return res.status(500).json("Hubo un error");
