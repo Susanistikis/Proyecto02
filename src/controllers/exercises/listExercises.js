b196ba5cbcbe9d1721f1718cc54aa23c25502a1a
Amalia <amaliaponcetoledo@gmail.com>
2023-09-07 08:31:38 +0200

    Filtro por favoritos

--- src/controllers/exercises/listExercises.js
+++ src/controllers/exercises/listExercises.js
@@ -1,32 +1,40 @@
 const getDb = require('../../db/getDb');
+
 async function listExercises(req, res) {
     let connection;
     try {
         const user_id = req.user.id;
-        const { name, muscleGroup } = req.query;
+        const { name, muscleGroup, is_favorite } = req.query;
 
         let query = `
-            SELECT
-                e.*,
-                CASE WHEN f.user_id IS NOT NULL THEN true ELSE false END AS is_favorite
+            SELECT e.*, 
+                   CASE WHEN f.user_id IS NOT NULL THEN true ELSE false END AS is_favorite
             FROM exercises e
             LEFT JOIN favorites f ON e.id = f.exercise_id AND f.user_id = ?
         `;
 
         const queryParams = [user_id];
 
-        if (name || muscleGroup) {
+        if (name || muscleGroup || is_favorite !== undefined) {
             query += ` WHERE`;
+
             if (name) {
                 query += ` (e.name LIKE ? OR e.description LIKE ?)`;
                 queryParams.push(`%${name}%`, `%${name}%`);
             }
-            if (name && muscleGroup) {
+
+            if (name && (muscleGroup || is_favorite !== undefined)) {
                 query += ` AND`;
             }
+
             if (muscleGroup) {
                 query += ` (e.muscleGroup LIKE ?)`;
                 queryParams.push(`%${muscleGroup}%`);
+            }
+
+            if (is_favorite !== undefined) {
+                query += ` (f.user_id IS NOT NULL AND f.user_id = ?)`;
+                queryParams.push(user_id);
             }
         }
 
@@ -35,20 +43,11 @@ async function listExercises(req, res) {
         const [results] = await connection.query(query, queryParams);
 
         if (results.length >= 1) {
-            if (results.length === 1) {
-        
-                return res.status(200).json({
-                    status: 'ok',
-                    message: 'Ejercicio encontrado',
-                    data: [results[0]],
-                });
-            } else {
-                return res.status(200).json({
-                    status: 'ok',
-                    message: 'Listado de ejercicios',
-                    data: results,
-                });
-            }
+            return res.status(200).json({
+                status: 'ok',
+                message: 'Listado de ejercicios',
+                data: results,
+            });
         } else {
             return res.status(404).json({
                 status: 'error',
