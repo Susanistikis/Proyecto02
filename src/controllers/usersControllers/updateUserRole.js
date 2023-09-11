6d25074b904962f183cda0f5d8da5a0bec8e2266
Susanistikis <susanam909@gmail.com>
2023-09-11 19:21:26 +0200

    updateUserRolFuncionando

--- src/controllers/usersControllers/updateUserRole.js
+++ src/controllers/usersControllers/updateUserRole.js
@@ -1,32 +1,77 @@
 const getDb = require('../../db/getDb');
+const jwt = require('jsonwebtoken');
 
-async function updateUserRole(req, res) {
-    let connection;
+const updateUserRole = async (req, res, next) => {
     try {
-        const user_id = req.user.id;
-        const { userRole } = req.body;
+        const token = req.headers.authorization;
 
-        connection = await getDb();
+        if (!token) {
+            return res
+                .status(401)
+                .json({ error: 'Token de autenticación no proporcionado.' });
+        }
 
-        await connection.query(
-            `
-            UPDATE users
-            SET userRole = ?
-            WHERE id = ?
-        `,
-            [user_id, userRole]
-        );
+        jwt.verify(token, process.env.SECRET, async err => {
+            if (err) {
+                return res
+                    .status(401)
+                    .json({ error: 'Token de autenticación inválido.' });
+            }
 
-        return res.status(200).json({
-            status: 'ok',
-            message: 'userRole actualizado exitosamente',
+            const adminUserId = req.user.id; // ID del administrador autenticado
+            const { userId, userRole } = req.body; // ID del usuario y el nuevo rol
+
+            const db = await getDb();
+
+            // Verificar si el usuario autenticado es un administrador
+            const [
+                admin,
+            ] = await db.query(
+                'SELECT * FROM users WHERE id = ? AND userRole = ?',
+                [adminUserId, 'admin']
+            );
+
+            if (!admin) {
+                db.release();
+                return res.status(401).json({
+                    status: 'error',
+                    message:
+                        'No tienes permiso para cambiar el rol de otros usuarios.',
+                });
+            }
+
+            const [user] = await db.query('SELECT * FROM users WHERE id = ?', [
+                userId,
+            ]);
+
+            if (!user) {
+                db.release();
+                return res.status(404).json({
+                    status: 'error',
+                    message: 'Usuario no encontrado',
+                });
+            }
+
+            const updateQuery = `
+                UPDATE users
+                SET userRole = ?
+                WHERE id = ?
+            `;
+
+            const updateValues = [userRole, userId];
+
+            await db.query(updateQuery, updateValues);
+
+            db.release();
+
+            return res.status(200).json({
+                status: 'ok',
+                message: 'userRole actualizado exitosamente',
+            });
         });
     } catch (error) {
-        console.error('Error:', error);
-        return res.status(500).json('Error en la consulta a la base de datos');
-    } finally {
-        if (connection) connection.release();
+        next(error);
     }
-}
+};
 
 module.exports = updateUserRole;
