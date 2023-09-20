6a1bbfb0c31a42f01ef99bf9189989cdf1907138
Amalia <amaliaponcetoledo@gmail.com>
2023-09-20 16:04:13 +0200

    Cambio

--- src/controllers/usersControllers/updateProfileController.js
+++ src/controllers/usersControllers/updateProfileController.js
@@ -26,26 +26,20 @@ const updateProfileController = async (req, res, next) => {
             photo,
         } = req.body;
         const userId = decodedToken.id;
+
         // Verifica si se proporcionó al menos un campo para actualizar el perfil.
-        if (
-            !name &&
-            !biography &&
-            !lastName &&
-            !birthDate &&
-            !address &&
-            !phone_number &&
-            !photo
-        ) {
+        if (!name && !biography && !lastName && !birthDate && !address && !phone_number && !photo) {
             return res.status(400).json({
-                error:
-                    'Debes proporcionar al menos un campo para actualizar el perfil.',
+                error: 'Debes proporcionar al menos un campo para actualizar el perfil.',
             });
         }
+
         const db = await getDb();
         try {
             // Construimos la consulta SQL para actualizar el perfil en la base de datos.
             let updateQuery = 'UPDATE users SET ';
             const updateValues = [];
+
             // Agregamos dinámicamente las columnas y valores a la consulta.
             if (name) {
                 updateQuery += 'name = ?, ';
@@ -75,11 +69,14 @@ const updateProfileController = async (req, res, next) => {
                 updateQuery += 'photo = ?, ';
                 updateValues.push(photo);
             }
+
             // Agregamos la parte final de la consulta.
             updateQuery += 'updated_at = CURRENT_TIMESTAMP WHERE id = ?';
             updateValues.push(userId);
+
             console.log('updateQuery:', updateQuery);
             console.log('updateValues:', updateValues);
+
             await db.query(updateQuery, updateValues);
             res.send({
                 status: 'ok',
